7d880a42a08818f2c1916f570badce6cb5e09961
Natali Angelova <natali.v.angelova@gmail.com>
2023-04-20 12:17:28 +0300

    Add all exercise to Map

--- src/Map1.java
+++ src/Map1.java
@@ -54,11 +54,41 @@ public class Map1 {
 
     //Ex. => mapAB2 -
     public Map<String, String> mapAB2(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b") && map.get("a").equals(map.get("b"))){
+            map.remove("a");
+            map.remove("b");
+        }
     return  map;
     }
 
+    //Ex. => mapAB3 -
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        if (!map.containsKey("a") && map.containsKey("b")){
+            map.put("a", map.get("b"));
+        }
+        if (!map.containsKey("b") && map.containsKey("a")){
+            map.put("b", map.get("a"));
+        }
+        return map;
+    }
+
     //Ex. => topping1 -
-    //Ex. => topping1 -
+    public Map<String, String> mapAB4(Map<String, String> map) {
+    if (map.containsKey("a") && map.containsKey("b")){
+        if (map.get("a").length() != map.get("b").length() ){
+            if (map.get("a").length() > map.get("b").length()) {
+                map.put("c", map.get("a"));
+            } else {
+                map.put("c", map.get("b"));
+            }
+        }else {
+            map.put("a", "");
+            map.put("b", "");
+        }
+    }
+    return map;
+    }
+
     //Ex. => topping1 -
     //Ex. => topping1 -
     public static void main(String[] args) {
