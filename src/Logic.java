b0aa94260047eaeb4c2fe805ea5cc159eeb6ecc1
Natali Angelova <natali.v.angelova@gmail.com>
2023-04-23 16:47:38 +0300

    Add logic exercise

--- src/Logic.java
+++ src/Logic.java
@@ -1,2 +1,17 @@
 public class Logic {
+    //Ex. => pairs -
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+       if (isWeekend){
+           if (cigars >= 40){
+               return true;
+           }
+       }else {
+           if (cigars >= 40 && cigars <=60){
+               return true;
+           }
+       }
+
+       return false;
+    }
+
 }
