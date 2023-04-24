1bd85ba1f80b33c405a834ccc2d49c0f6660f23c
Natali Angelova <natali.v.angelova@gmail.com>
2023-04-24 23:21:48 +0300

    Update Logical exercise

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Logic {
     //Ex. => pairs -
     public boolean cigarParty(int cigars, boolean isWeekend) {
@@ -10,8 +13,53 @@ public class Logic {
                return true;
            }
        }
-
        return false;
     }
 
+    //Ex. => dateFashion -
+    public int dateFashion(int you, int date) {
+        if (you <= 2 || date <= 2){
+            return 0;
+        }else if (you >= 8 || date >= 8){
+            return 2;
+        }else {
+            return 1;
+        }
+    }
+    //Ex. => dateFashion -
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (isSummer){
+            return temp >= 60 && temp <= 100;
+        }else {
+            return temp >= 60 && temp <= 90;
+        }
+    }
+
+    //Ex. => caughtSpeeding -
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if (isBirthday) {
+            speed -= 5;
+        }
+
+        if (speed <= 60) {
+            return 0;
+        } else if (speed <= 80) {
+            return 1;
+        } else {
+            return 2;
+        }
+
+    }
+
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+    //Ex. => dateFashion -
+
+
 }
+
+
