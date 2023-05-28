4f931dff064cc3a50aff74376ac829b0e1e525ed
Natali Angelova <natali.v.angelova@gmail.com>
2023-05-28 22:27:47 +0300

    Add addStar

--- src/Functional.java
+++ src/Functional.java
@@ -4,11 +4,17 @@ import java.util.stream.Collectors;
 public class Functional {
     //Ex. => doubling -
     public List<Integer> doubling(List<Integer> nums) {
-        return nums.stream().map(x -> x*2).collect(Collectors.toList());
+        return nums.stream().map(x -> x * 2).collect(Collectors.toList());
     }
-    //Ex. => doubling -
+
+    //Ex. => square -
     public List<Integer> square(List<Integer> nums) {
-        return nums.stream().map(num -> num*num).collect(Collectors.toList());
+        return nums.stream().map(num -> num * num).collect(Collectors.toList());
+    }
+
+    //Ex. => addStar -
+    public List<String> addStar(List<String> strings) {
+        return strings.stream().map(s -> s + "*").collect(Collectors.toList());
     }
 
 
